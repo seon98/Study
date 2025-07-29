4e87680b237c638e7ce8f1a0a01ae8a88265365c
선희철 <seonheecher4806@gmail.com>
2025-07-29 14:38:38 +0900

    OS_Study

---
+++ Study/18.js
@@ -0,0 +1,16 @@
+
+let players = {
+    boys : {
+        Bergkamp : "Striker"
+    }
+}
+
+let persons = players
+
+players = ["Son", "Park"]
+
+let human = persons.boys
+
+persons = "persons"
+
+human = null
